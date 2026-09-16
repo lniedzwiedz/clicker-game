cf6ae937f0606be66384fdf668231cb7c7529be1
Liliana Niedzwiedz <liliananiedzwiedz@gmail.com>
2026-09-16 13:35:34 +0200

    refactor: part 24: extract statisticFraud.css from home.css

--- js/main.js
+++ js/main.js
@@ -16,4 +16,4 @@ const viewFooter = new ViewFooter();
 viewFooter.createFooter();
 
 
-console.log("Clicker game: version 20260916v50_branch_class_23");
+console.log("Clicker game: version 20260916v50_branch_class_24");
